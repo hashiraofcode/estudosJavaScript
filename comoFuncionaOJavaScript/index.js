9a54ed6a4b3f96ec3eadacb15fbc816052da17c2
Valmir-Neto <netovalmirr@gmail.com>
2025-05-28 11:08:41 -0300

    aplicando consdicionais no código principal

--- comoFuncionaOJavaScript/index.js
+++ comoFuncionaOJavaScript/index.js
@@ -12,6 +12,18 @@ function calculadorDeImc(height, weigth) {
  return(resultado)
 }
 console.log(calculadorDeImc(183,96))
+// função que classifica o imc
+function classificandoImc(imc) {
+   if (imc < 18.5) {
+      return "Abaixo do peso"
+   } else if (imc > 18.5 && imc < 25) {
+      return "Peso normal"
+   } else if (imc > 25 && imc < 30) {
+      return "Sobrepeso"
+   } else {
+      return "Obesidade"
+   }
+}
 
 // outra maneira é misturando objetos e a manipulação dos mesmos
 function calculadorDeImc2(person) {
@@ -21,6 +33,7 @@ function calculadorDeImc2(person) {
  let idade = person.idade
  altura *= fatorDeConvercao
  let imc = peso / (altura * altura)
+ let classificacao = classificandoImc(imc)
  let resultado = `O valor do IMC deste indivíduo é ${imc}`
  const isHealth = true
  return{
@@ -28,6 +41,7 @@ function calculadorDeImc2(person) {
     altura: `${altura}m`,
     peso: `${peso}Kg`,
     resultado,
+    classificacao,
     isHealth
  }
 }
@@ -41,3 +55,4 @@ function calculadorDeImc2(person) {
 console.log(calculadorDeImc2(person))
 
 //# a terceira mudança que eu vou fazer no código, aprendi sobre estruturas de controle if/else e como fazer escolhas a partir de operções lógicas permitindo que o código tome decisões de maneira dinâmica;
+// eu posso colocar funções dentro de outras e utilizar os dados dentro das mesmas como parâmetro com o pórem de que a função que você vai utilizar já esteja criada quando você precisar dela em outra parte posterior do código caso a função em que você vai emprega-la seja anterior vai dar erro, pois essa função não exitiria.
