7b8f2e93d4037979ef6f4761bd93238913533496
Valmir-Neto <netovalmirr@gmail.com>
2025-05-28 20:33:29 -0300

    aprendendo switch e boas práticas de código

--- aprendendoEstruturasDeControle/ex2.js
+++ aprendendoEstruturasDeControle/ex2.js
@@ -14,11 +14,44 @@ if (hora >= 6 && hora < 12) {
 } else if (hora >= 12 && hora < 18) {
     console.log("Boa tarde!!")
 } else if (hora >= 18 && hora < 24) {
- console.log("Boa noite!!")
-} else if(hora == 0) {
+    console.log("Boa noite!!")
+} else if (hora == 0) {
     console.log("Está ficando tarde")
 } else if (hora < 6 && hora > 0) {
     console.log("É madrugada o que você está fazendo acordado??? ;-;")
 } else {
     console.log("essa hora tá certa??? ;-;")
 }
+
+// aprendendo a estrutura de controle switch, que funcioana de uma maneira diferente do if/else. A estrutura em questão pode especificar o que se é esperado da condicao como um valor string por exemplo, o que o diferencia do if/else que faz uma relação entre dois dados pretendendo obter uma resposta de verdadeiro ou falso o que permite um comparação mais ampla, ja no switch os possiveiss resultados e suas respostas tem que ser especificados.
+
+let bebidaEscolhida = "fanta"
+switch (bebidaEscolhida) {
+    case "Coca-cola": console.log("mandando Coca-cola")
+        break;
+    case "fanta": console.log("mandando fanta")
+        break;
+    case "pepsi": console.log("mandando pepsi")
+        break;
+    default:
+        console.log("água")
+}
+
+let diaDaSemana = "quarta"
+switch (diaDaSemana) {
+    case "segunda": console.log("hoje é dia de Cappuccino")
+        break;
+    case "terça": console.log("hoje é dia de Latte")
+        break;
+    case "quarta": console.log("hoje é dia de Moccha")
+        break;
+    case "quinta": console.log("hoje é dia de Expresso")
+        break;
+    case "sexta": console.log("hoje é dia de Macchiato")
+        break;
+    case "sábado":
+    case "domingo": console.log("hoje é dia de Café especial")
+    break;
+    default: console.log("Dia da semana inválido")
+}
+// no desenvolvimento é muito comum que o back-end crie status ao invês de a informação propriamente esperada, na maioria das vezes por conta de uma questão de organização, seja por questão do idioma em que a aplicação esta sendo desenvolvida que esta diferente dos valores no banco de dados. Por isso é muito comum que retornem estatus e nós do front utilizamos o swich para retornar o que precisamos a partir deste.
--- comoFuncionaOJavaScript/index.js
+++ comoFuncionaOJavaScript/index.js
@@ -15,16 +15,35 @@ console.log(calculadorDeImc(183,96))
 // função que classifica o imc
 function classificandoImc(imc) {
    if (imc < 18.5) {
-      return "Abaixo do peso"
+      return "UNDERWEIGTH"
    } else if (imc > 18.5 && imc < 25) {
-      return "Peso normal"
+      return "REGULAR"
    } else if (imc > 25 && imc < 30) {
-      return "Sobrepeso"
+      return "OVERWEIGTH"
    } else {
-      return "Obesidade"
+      return "OBESITY"
    }
 }
-
+let IMC_STATUS_DICT = {
+   UNDERWEIGTH: "UNDERWEIGTH",
+   REGULAR: "REGULAR",
+   OVERWIGTH: "OVERWEIGTH",
+   OBESITY: "OBESITY",
+}
+// switch que pega os dados do back e trasforma eles na informação que preciso
+   function GET_IMC_STATUS(classificacao) {
+      switch (classificacao) {
+         case IMC_STATUS_DICT.UNDERWEIGTH: return "Esta pessoa está abaixo do peso"
+         break;
+         case IMC_STATUS_DICT.REGULAR: return "Esta pessoa está no seu peso ideal"
+         break;
+         case IMC_STATUS_DICT.OVERWIGTH: return "Esta pessoa está acima do peso"
+         break;
+         case IMC_STATUS_DICT.OBESITY: return "Esta pessoa se encontra num grau de obesidade"
+         break;
+         default: return "Verifique as informações preenchidas"
+      }
+   }
 // outra maneira é misturando objetos e a manipulação dos mesmos
 function calculadorDeImc2(person) {
  const fatorDeConvercao = 0.01
@@ -34,6 +53,7 @@ function calculadorDeImc2(person) {
  altura *= fatorDeConvercao
  let imc = peso / (altura * altura)
  let classificacao = classificandoImc(imc)
+ let getImcStatus = GET_IMC_STATUS(classificacao)
  let resultado = `O valor do IMC deste indivíduo é ${imc}`
  const isHealth = true
  return{
@@ -41,7 +61,7 @@ function calculadorDeImc2(person) {
     altura: `${altura}m`,
     peso: `${peso}Kg`,
     resultado,
-    classificacao,
+    getImcStatus,
     isHealth
  }
 }
@@ -55,4 +75,14 @@ function calculadorDeImc2(person) {
 console.log(calculadorDeImc2(person))
 
 //# a terceira mudança que eu vou fazer no código, aprendi sobre estruturas de controle if/else e como fazer escolhas a partir de operções lógicas permitindo que o código tome decisões de maneira dinâmica;
+
 // eu posso colocar funções dentro de outras e utilizar os dados dentro das mesmas como parâmetro com o pórem de que a função que você vai utilizar já esteja criada quando você precisar dela em outra parte posterior do código caso a função em que você vai emprega-la seja anterior vai dar erro, pois essa função não exitiria.
+
+// no desenvolvimento é muito comum que o back-end crie status ao invês de a informação propriamente esperada, na maioria das vezes por conta de uma questão de organização, seja por questão do idioma em que a aplicação esta sendo desenvolvida que esta diferente dos valores no banco de dados. Por isso é muito comum que retornem estatus e nós do front utilizamos o swich para retornar o que precisamos a partir deste.
+
+// Outra prática comum no desenvolvimento é a criação de dicionarios para evitar erros de escrita e assim causar um bug no código. É considerado uma boa prática.
+// let cardapio = { 
+ // MOCCHA: "MOCCHA"
+ // LEITE : "leite"
+//}
+// a organização do objeto vai depender muito da escrita que o seu código precisa para funcionar desde que não esteja errado não há problema
