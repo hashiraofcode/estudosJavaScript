598d95d0f09fccdacb46bcfa27a0daaa7e2195c4
Valmir-Neto <netovalmirr@gmail.com>
2025-05-31 15:57:13 -0300

    Aprendendo sobre a função setInterval, e fazendo um timer;

--- AprendendoFuncoes/ex5.js
+++ AprendendoFuncoes/ex5.js
@@ -21,4 +21,27 @@ function parOuImpar(number) {
     return number % 2 === 0
 }
 
-console.log(parOuImpar(2))
+// console.log(parOuImpar(2))
+
+// Fazendo um contador regressivo utilizando uma função e o setInterval. setInterval é uma função do javaScript que recebe uma função e uma quantidade de tempo (em milisegundos) como argumentos; Assim ele executa a função passada de acordo com a quantidade de tempo desejado;
+// Esta função serve para se caso eu quiser colocar o tempo em segundos e ele é trasformado em milisegundos para satisfazer o argumento da função;
+
+let getTimeMillisecunds = function (timeInSec) {
+    return timeInSec * 1000
+}
+
+//função que vai fazer o timer
+function timer(tempo) {
+    let getTempo = tempo
+    let intervalTime = setInterval(function () {
+        console.log(getTempo)
+        if (getTempo === 0) {
+            clearInterval(intervalTime)
+        } else {
+            getTempo--
+        }
+    }, getTimeMillisecunds(1))
+}
+
+
+// importante para quando queromos executar algo mais de uma vez com um intervalo de tempo; também podemos fazer com que ele pare utilizando a função clearIntarval() que recebe o setInterval em questão como argumento. E para especificar qual intervalo estamos falando colocamos ele dentro de uma variavel. e utilizamos um condicional para decidir de forma dinâmica quando devemos para o timer.
