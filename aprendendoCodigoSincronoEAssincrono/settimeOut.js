0ff4d9a96d0471aa7d0d9928a095bdf1c887d87b
Valmir-Neto <netovalmirr@gmail.com>
2025-06-09 17:27:37 -0300

    aprendendo setTimeout

---
+++ aprendendoCodigoSincronoEAssincrono/settimeOut.js
@@ -0,0 +1,6 @@
+// setTimeOut tem a função de executar um código depois de um tempo previsto definido em milisegundos, muito utilizada em programação assincrona onde a gente precisa que o código continue rolando mesmo sem executar um trecho ou um bloco ou também até um conjunto de funções. Os mesmos só serão executados após expirar a contagem da função
+console.log("em dois segundos comprimente o meu amigo")
+
+setTimeout(() => {
+    console.log("olá amigo")
+}, 2000)
